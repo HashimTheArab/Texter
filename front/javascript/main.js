6a1371f7f39a276b99795e446273478667ca9edd
Hashim <13991048+Prim69@users.noreply.github.com>
2021-10-02 01:57:11 -0400

    init

---
+++ front/javascript/main.js
@@ -0,0 +1,44 @@
+
+let username = "";
+let ip = 'ip of host'; // make sure to change this to your ip or the ip of whatever you're hosting it on
+let socket = new WebSocket(`ws://${ip}:8080/ws`);
+
+socket.onopen = function(){
+  console.log("Socket Connected!");
+};
+
+socket.onmessage = function(ev) {
+    document.getElementById('texts').innerHTML += (ev.data + "<br>");
+};
+
+function selectName(){
+    const name = document.getElementById('chooseName');
+    const button = document.getElementById('confirmName');
+    if(name.value.trim().length < 1 || name.value.length > 30){
+        //document.getElementById('shortWarning').innerHTML = "Your name must between 1-30 characters!";
+        return;
+    }
+    button.style.display = "none";
+    name.placeholder = "Send a Message!";
+    document.getElementById('shownName').innerHTML = 'Your Name: ' + name.value;
+    name.spellcheck = true;
+    /*name.addEventListener("input", function(){
+        if(username !== ""){
+            socket.send(`${username} (Typing): ${name.value}`);
+        }
+    });*/
+    name.addEventListener("keydown", function(event){
+        if(event.code === "Enter" && username !== "" && name.value !== ""){
+            socket.send(`${username}: ${name.value} - ${new Date().toLocaleString("en-US", { dataStyle: "full", timeStyle: "short"})}`);
+            name.value = "";
+        }
+    });
+    username = name.value;
+    name.value = "";
+    socket.send("<br><div style='color: lightseagreen; background-color: aquamarine'>" + username + " has connected!</div>");
+    socket.send(`<div style='color: red; background-color: aquamarine'> ${username}'s screen resolution is ${screen.width}x${screen.height} (color depth: ${screen.pixelDepth} | pixel depth: ${screen.pixelDepth} | orientation: ${screen.orientation.type}) </div>`);
+}
+
+function sends(){
+    socket.send(document.getElementById('chooseName').value);
+}
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"strconv"
+)
+
+var clients = map[*websocket.Conn]bool{}
+var upgrader = websocket.Upgrader{}
+var broadcast = make(chan []byte)
+var btype = make(chan int)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./front")))
+	http.HandleFunc("/ws", func(writer http.ResponseWriter, request *http.Request) {
+		upgrader.CheckOrigin = func(r *http.Request) bool {
+			return true
+		}
+		conn, err := upgrader.Upgrade(writer, request, nil)
+		if err != nil {
+			panic(err)
+		}
+		defer func(ws *websocket.Conn) {
+			err := ws.Close()
+			if err != nil {
+				panic(err)
+			}
+		}(conn)
+		clients[conn] = true
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			broadcast <- msg
+			btype <- msgType
+			println("Message Received (" + strconv.Itoa(msgType) + "): " + string(msg))
+		}
+	})
+	go func(){
+		for {
+			msg := <- broadcast
+			t := <- btype
+			for client := range clients {
+				if err := client.WriteMessage(t, msg); err != nil {
+					_ = client.Close()
+					delete(clients, client)
+				}
+			}
+		}
+	}()
+
+	println("Starting server!")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
